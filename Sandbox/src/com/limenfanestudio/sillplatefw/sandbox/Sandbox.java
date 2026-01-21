6bee0e6d3942a60698d1b9f17fd822ab111dae83
LimenfaneStudio <info@limenfanestudio.com>
2026-01-21 17:58:44 -0400

    Added Java projects and tested build environment

---
+++ Sandbox/src/com/limenfanestudio/sillplatefw/sandbox/Sandbox.java
@@ -0,0 +1,20 @@
+/*
+ * File:		Sandbox.java
+ * Author:		Limenfane Studio (info@limenfanestudio.com)
+ * Created:		2026.01.21
+ * Purpose:		The main class of the Sandbox project for testing the Sillplate
+ * 				Framework
+ */
+
+package com.limenfanestudio.sillplatefw.sandbox;
+
+import com.limenfanestudio.sillplatefw.SillplateFW;
+
+public class Sandbox {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SillplateFW.foo();
+	}
+
+}
